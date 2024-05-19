ac2da7e063b9058752558aadd859f9778b1d0d2e
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-05-19 13:46:47 +0200

    structure

---
+++ GithubBlog/src/App.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return (
+    <h1>Blog</h1>
+    )
+}
---
+++ GithubBlog/src/styles/themes/default.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  blue: '#3294f8',
+  title: '#e7edf4',
+  subtitle: '#c4d4e3',
+  text: '#afc2d4',
+  span: '#7b96b2',
+  label: '#3a536b',
+  border: '#1c2f41',
+  post: '#112131',
+  profile: '#0b1b2b',
+  background: '#071422',
+  input: '#040f1a'
+} as const
